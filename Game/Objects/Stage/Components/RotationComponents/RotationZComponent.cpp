55e0e4ef1d04b16ffaace9d14694aa5c8521bd39
sugawara yuto <117239251+nattoSG09@users.noreply.github.com>
2024-05-05 16:42:07 +0900

    // 角度の上限下限を360 > 0 < -360に設定

--- Game/Objects/Stage/Components/RotationComponents/RotationZComponent.cpp
+++ Game/Objects/Stage/Components/RotationComponents/RotationZComponent.cpp
@@ -17,7 +17,15 @@ void RotationZComponent::Update()
 {
 	// Z軸に回転させる
 	angle_ += rotateSpeed_;
-	if(rotateSpeed_ > 0)holder_->SetRotateZ(angle_);
+
+	// 回転速度の値が正の値である かつ 角度が360度を上回った時、角度を０に戻す
+	if (!std::signbit(rotateSpeed_) && angle_ >= 360.f)angle_ = 0.f;
+
+	// 回転速度の値が負の値である かつ 角度が-360度を上回った時、角度を０に戻す
+	if (std::signbit(rotateSpeed_) && angle_ <= -360.f)angle_ = 0.f;
+
+	// 回転速度が０ではない時、角度を適応させる
+	if (rotateSpeed_ != 0.f)holder_->SetRotateZ(angle_);
 }
 
 void RotationZComponent::Release()

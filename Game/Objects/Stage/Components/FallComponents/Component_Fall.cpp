9e685bfc2dd3f711605815ac83884c270b40c263
KikuchiAkihiro <game.sugokudaisuki@gmail.com>
2024-05-15 10:10:33 +0900

    中身書き始め

--- Game/Objects/Stage/Components/FallComponents/Component_Fall.cpp
+++ Game/Objects/Stage/Components/FallComponents/Component_Fall.cpp
@@ -1,1 +1,34 @@
 #include "Component_Fall.h"
+
+//インクルード
+#include "../../StageObject.h"
+
+Component_Fall::Component_Fall(StageObject* _holder)
+	:Component(_holder, "Component_Fall", Fall), fallSpeed_(0)
+{
+}
+
+void Component_Fall::Initialize()
+{
+}
+
+void Component_Fall::Update()
+{
+
+}
+
+void Component_Fall::Release()
+{
+}
+
+void Component_Fall::Save(json& _saveObj)
+{
+}
+
+void Component_Fall::Load(json& _loadObj)
+{
+}
+
+void Component_Fall::DrawData()
+{
+}
--- Game/Objects/Stage/Components/FallComponents/Component_Fall.h
+++ Game/Objects/Stage/Components/FallComponents/Component_Fall.h
@@ -1,5 +1,49 @@
 #pragma once
-class Component_Fall
+
+#include "../Component.h"
+
+class Component_Fall : public Component
 {
+private:
+	float fallSpeed_; //降下速度
+	float riseSpeed_; //上昇速度
+public:
+	///<summary>
+	/// コンストラクタ
+	///</summary>
+	///<param name="_holder">保有者</param>
+	Component_Fall(StageObject* _holder);
+
+	///<summary>
+	/// 初期化
+	///</summary>
+	void Initialize() override;
+
+	///<summary>
+	/// 更新
+	///</summary>
+	void Update() override;
+
+	/// <summary>
+	/// 開放
+	/// </summary>
+	void Release() override;
+
+	/// <summary>
+	/// 保存
+	/// </summary>
+	/// <param name="_saveObj">保存情報</param>
+	void Save(json& _saveObj) override;
+
+	/// <summary>
+	/// 読込
+	/// </summary>
+	/// <param name="_loadObj">読込情報</param>
+	void Load(json& _loadObj) override;
+
+	/// <summary>
+	/// ImGui表示
+	/// </summary>
+	void DrawData() override;
 };
 

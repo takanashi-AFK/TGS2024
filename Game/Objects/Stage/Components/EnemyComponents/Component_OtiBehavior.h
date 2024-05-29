6d3887a2d84bb3e1cf2952a1bf13d735c78d8619
Nayet <n4shoya@icloud.com>
2024-05-30 01:35:05 +0900

    Fix : OtiBehavior

--- Game/Objects/Stage/Components/EnemyComponents/Component_OtiBehavior.h
+++ Game/Objects/Stage/Components/EnemyComponents/Component_OtiBehavior.h
@@ -6,14 +6,42 @@ class Component_OtiBehavior : public Component
 private:
 	
 public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	/// <param name="_holder">保有者</param>
 	Component_OtiBehavior(StageObject* _holder);
 	
+	/// <summary>
+	/// 初期化
+	/// </summary>
 	void Initialize() override;
+
+	/// <summary>
+	/// 更新
+	/// </summary>
 	void Update() override;
+
+	/// <summary>
+	/// 解放
+	/// </summary>
 	void Release() override;
 
+	/// <summary>
+	/// 保存
+	/// </summary>
+	/// <param name="_saveObj">保存情報</param>
 	void Save(json& _saveObj) override;
+
+	/// <summary>
+	/// 読込
+	/// </summary>
+	/// <param name="_loadObj">読込情報</param>
 	void Load(json& _loadObj) override;
+
+	/// <summary>
+	/// ImGuiパネル表示
+	/// </summary>
 	void DrawData() override;
 };
 

1512c7760b81da6d6ad775bb100c5977088a5502
sato-yuito <220079@jc-21.jp>
2024-07-09 21:54:53 +0900

    できたと思う

--- Game/Scenes/Scene_StageSelect.cpp
+++ Game/Scenes/Scene_StageSelect.cpp
@@ -9,10 +9,10 @@
 #include"../../Engine/DirectX/Direct3D.h"
 
 namespace {
-	 float maxButtonMove_ = 1.5f;
+	float maxButtonMove_ = 1.5f;
 }
 Scene_StageSelect::Scene_StageSelect(GameObject* parent)
-	: GameObject(parent, "Scene_StageSelect"), isRightButtonMoving_(false), isLeftSelectButtonMoving_(false),moveselectButton(0.f),StageIndex(0),easingfunc_()
+	: GameObject(parent, "Scene_StageSelect"), isRightButtonMoving_(false), isLeftSelectButtonMoving_(false), moveselectButton(0.f), StageIndex(0), easingfunc_()
 {
 }
 
@@ -26,7 +26,7 @@ void Scene_StageSelect::Initialize()
 		uipanel->Load(uiData);
 	}
 
-	
+
 	stageImages.push_back(dynamic_cast<UIButton*>(uipanel->GetUIObject("StageSelectButton1")));
 	stageImages.push_back(dynamic_cast<UIButton*>(uipanel->GetUIObject("StageSelectButton2")));
 }
@@ -51,7 +51,7 @@ void Scene_StageSelect::Update()
 #endif // DEBUG
 
 	//---------ステージ選択画面がスクロール式で移動するようにする-----------------------------------
-	
+
 	//stageSelectButtonが押されたらPlayシーンに移動
 	UIButton* stageSelectButton = stageImages[StageIndex];
 	if (stageSelectButton == nullptr)return;
@@ -67,22 +67,22 @@ void Scene_StageSelect::Update()
 		isRightButtonMoving_ = true;
 		StageIndex = (StageIndex + 1) % stageImages.size();
 	}
-	
+
 	//Buttonが押されたら-方向に移動し画像を入れ替える
 	UIButton* backButton = dynamic_cast<UIButton*>(uipanel->GetUIObject("BuckButton"));
 	if (backButton == nullptr)return;
 	if (backButton->OnClick()) {
 		isLeftSelectButtonMoving_ = true;
-		
+
 		StageIndex = (StageIndex - 1 + stageImages.size()) % stageImages.size();
 	}
 	if (isRightButtonMoving_) {
-		RightButtonMoving();
-	 }
-	 if (isLeftSelectButtonMoving_) {
-		LeftButtonMoving();
+		MoveButtons(true);
 	}
-	
+	if (isLeftSelectButtonMoving_) {
+		MoveButtons(false);
+	}
+
 	/*for (size_t i = 0; i < stageImages.size(); ++i)
 	{
 		UIButton* button = stageImages[i];
@@ -91,7 +91,7 @@ void Scene_StageSelect::Update()
 			ImGui::Text("Button %zu Pos: %.2f", i, button->GetPosition().x);
 		}
 	}*/
-	
+
 }
 
 
@@ -103,64 +103,66 @@ void Scene_StageSelect::Release()
 {
 }
 
-void Scene_StageSelect::RightButtonMoving()
-{
-
-	
-		for (int i = 0; i < stageImages.size(); ++i) {
-
-			UIButton* button = stageImages[i];
-
-			//そのi番目にあるボタンの画像のx座標をを取得し変数名に代入
-			float ButtonPos = button->GetPosition().x;
-
-			moveselectButton += 0.01;
-
-			button->SetPosition(ButtonPos += maxButtonMove_ * Direct3D::EaseFunc[easingfunc_](moveselectButton), stageImages[i]->GetPosition().y, stageImages[i]->GetPosition().z);
-
-		}
-
-		if (stageImages[1]->GetPosition().x >= 0 || moveselectButton >= maxButtonMove_) {
-			moveselectButton = 0;
-			isRightButtonMoving_ = false;
-
-			// 1番目の画像を0の位置に固定
-			stageImages[1]->SetPosition(0, stageImages[1]->GetPosition().y, stageImages[1]->GetPosition().z);
-		}
-	
-}
-
-void Scene_StageSelect::LeftButtonMoving()
-{
-	for (int i = 0; i < stageImages.size(); ++i) {
-
-			UIButton* button = stageImages[i];
-
-			//そのi番目にあるボタンの画像のx座標をを取得し変数名に代入
-			float ButtonPos = button->GetPosition().x;
-
-			moveselectButton += 0.01;
-
-			button->SetPosition(ButtonPos -= maxButtonMove_ * Direct3D::EaseFunc[easingfunc_](moveselectButton), stageImages[i]->GetPosition().y, stageImages[i]->GetPosition().z);
-
-			if (stageImages[0]->GetPosition().x <= 0 || moveselectButton <= maxButtonMove_) {
-				moveselectButton = 0;
-				isLeftSelectButtonMoving_ = false;
-
-				// 1番目の画像を0の位置に固定
-				stageImages[0]->SetPosition(0, stageImages[0]->GetPosition().y, stageImages[0]->GetPosition().z);
-			}
-	}
-}
+//void Scene_StageSelect::RightButtonMoving()
+//{
+//
+//	
+//		for (int i = 0; i < stageImages.size(); ++i) {
+//
+//			UIButton* button = stageImages[i];
+//
+//			//そのi番目にあるボタンの画像のx座標をを取得し変数名に代入
+//			float ButtonPos = button->GetPosition().x;
+//
+//			moveselectButton += 0.01;
+//
+//			button->SetPosition(ButtonPos += maxButtonMove_ * Direct3D::EaseFunc[easingfunc_](moveselectButton), stageImages[i]->GetPosition().y, stageImages[i]->GetPosition().z);
+//
+//		}
+//
+//		if (stageImages[1]->GetPosition().x >= 0 || moveselectButton >= maxButtonMove_) {
+//			moveselectButton = 0;
+//			isRightButtonMoving_ = false;
+//
+//			// 1番目の画像を0の位置に固定
+//			stageImages[1]->SetPosition(0, stageImages[1]->GetPosition().y, stageImages[1]->GetPosition().z);
+//		}
+//	
+//}
+//
+//void Scene_StageSelect::LeftButtonMoving()
+//{
+//	for (int i = 0; i < stageImages.size(); ++i) {
+//
+//			UIButton* button = stageImages[i];
+//
+//			//そのi番目にあるボタンの画像のx座標をを取得し変数名に代入
+//			float ButtonPos = button->GetPosition().x;
+//
+//			moveselectButton += 0.01;
+//
+//			button->SetPosition(ButtonPos -= maxButtonMove_ * Direct3D::EaseFunc[easingfunc_](moveselectButton), stageImages[i]->GetPosition().y, stageImages[i]->GetPosition().z);
+//
+//			if (stageImages[0]->GetPosition().x <= 0 || moveselectButton <= maxButtonMove_) {
+//				moveselectButton = 0;
+//				isLeftSelectButtonMoving_ = false;
+//
+//				// 1番目の画像を0の位置に固定
+//				stageImages[0]->SetPosition(0, stageImages[0]->GetPosition().y, stageImages[0]->GetPosition().z);
+//			}
+//	}
+//}
 
 void Scene_StageSelect::MoveButtons(bool _moving)
 {
+
 	for (int i = 0; i < stageImages.size(); ++i) {
 
 		UIButton* button = stageImages[i];
 		float ButtonPos = button->GetPosition().x;
 		moveselectButton += 0.01;
 
+		//trueだったら右に行くそれ以外は左に移動
 		if (_moving) {
 			button->SetPosition(ButtonPos += maxButtonMove_ * Direct3D::EaseFunc[easingfunc_](moveselectButton), stageImages[i]->GetPosition().y, stageImages[i]->GetPosition().z);
 		}
@@ -169,10 +171,17 @@ void Scene_StageSelect::MoveButtons(bool _moving)
 		}
 	}
 	if (stageImages[1]->GetPosition().x >= 0 || moveselectButton >= maxButtonMove_
-		|| stageImages[0]->GetPosition().x <= 0 || moveselectButton <= maxButtonMove_) {
+		|| stageImages[0]->GetPosition().x <= 0) {
 		moveselectButton = 0;
 		if (_moving) {
-
+			isRightButtonMoving_ = false;
+			// 1番目の画像を0の位置に固定
+			stageImages[1]->SetPosition(0, stageImages[1]->GetPosition().y, stageImages[1]->GetPosition().z);
+		}
+		else {
+			isLeftSelectButtonMoving_ = false;
+			// 1番目の画像を0の位置に固定
+			stageImages[0]->SetPosition(0, stageImages[0]->GetPosition().y, stageImages[0]->GetPosition().z);
 		}
 	}
 }

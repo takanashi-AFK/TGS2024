689f231c8d8ed221524ad21879cf59ba56be1eb5
Nayet <n4shoya@icloud.com>
2024-07-24 13:36:43 +0900

    なんでモーションが動かないかわからない、いったんセーブ

--- Game/Objects/Stage/Components/MotionComponent/Component_PlayerMotion.cpp
+++ Game/Objects/Stage/Components/MotionComponent/Component_PlayerMotion.cpp
@@ -1,6 +1,7 @@
 #include "../../../../../Engine/ResourceManager/Model.h"
 #include "Component_PlayerMotion.h"
 #include "../../Stage.h"
+#include "../../../../../Engine/ImGui/imgui.h"
 
 Component_PlayerMotion::Component_PlayerMotion(string _name, StageObject* _holder,  Component* _parent)
 	:Component_Motion(_name, _holder, PlayerMotion, _parent)
@@ -12,41 +13,32 @@ void Component_PlayerMotion::Initialize()
 	motionModelMap_.insert(std::make_pair(PSTATE_WALK, Model::Load("Models/Player/Running.fbx")));
 	// モデルのロード
 	auto stageObjectList = ((Stage*)holder_)->GetStageObjects();
-
-	// このPlayerBehaviorが取得できていないからこのコード見直す
-	// holder_がもってるPlayerBehaviorもってくればいいじゃんね
-	for (auto stageObject : stageObjectList) {
-		auto componentVector = stageObject->FindComponent(PlayerBehavior);
-		if (componentVector.empty()) continue;
-		for (auto pl : componentVector) {
-			if (pl == nullptr)return;
-			playerBehavior_ = ((Component_PlayerBehavior*)pl);
-		}
-	}
 }
 
 void Component_PlayerMotion::Update()
 {
 	static 	bool isAnimationNow_ = false;
-	if (playerBehavior_ == nullptr)return;
+	if (parent_ == nullptr)return;
 	// 現在のStateを取得
-	state_ = playerBehavior_->GetState();
+	state_ = ((Component_PlayerBehavior*)parent_)->GetState();
 
 	// ここうまくいってないから確認お願い
 	if (state_ == PSTATE_WALK ) {
 		// playerBehaviorのモデルハンドルを変える
-		if (((StageObject*)playerBehavior_)->GetModelHandle() == motionModelMap_[PSTATE_WALK])return;
-		((StageObject*)playerBehavior_)->SetModelHandle(motionModelMap_[PSTATE_WALK]);
+		((StageObject*)parent_)->SetModelHandle(motionModelMap_[PSTATE_WALK]);
 
 		// モーション再生
-		if (isAnimationNow_ == false) 
-		Model::SetAnimFrame(((StageObject*)playerBehavior_)->GetModelHandle(), 0, 40, 1);
+		if (isAnimationNow_ == false){
+		Model::SetAnimFrame(((StageObject*)parent_)->GetModelHandle(), 0, 40, 1);
 		isAnimationNow_ = true;
+		}
 		
 	}
 	else if(state_ != PSTATE_WALK) {
 		isAnimationNow_ = false;
 	}
+
+	ImGui::Text(isAnimationNow_?"isAnimationNow_ :true":"isAnimationNow_ :false");
 }
 
 void Component_PlayerMotion::Release()
@@ -55,6 +47,7 @@ void Component_PlayerMotion::Release()
 
 void Component_PlayerMotion::DrawData()
 {
+
 }
 
 // playerStateのアドレスを取得

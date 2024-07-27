1e37c1ad7e07428691f08071e8618dcb13540043
Nayet <n4shoya@icloud.com>
2024-07-28 04:00:21 +0900

    pAnim モデルごとのアニメーションの打ち分けできた！！！！！最高の気分 あとはShootからIdleに移行するタイミングをもーしょんが終わった瞬間にすればたぶんいい感じになると思う

--- Game/Objects/Stage/Components/MotionComponent/Component_PlayerMotion.cpp
+++ Game/Objects/Stage/Components/MotionComponent/Component_PlayerMotion.cpp
@@ -3,52 +3,72 @@
 #include "../../Stage.h"
 #include "../../../../../Engine/ImGui/imgui.h"
 
-Component_PlayerMotion::Component_PlayerMotion(string _name, StageObject* _holder,  Component* _parent)
-	:Component_Motion(_name, _holder, PlayerMotion, _parent)
+Component_PlayerMotion::Component_PlayerMotion(string _name, StageObject* _holder, Component* _parent)
+    :Component_Motion(_name, _holder, PlayerMotion, _parent), currentFrame_(0), animationEndFrame_(0)
 {
 }
 
 void Component_PlayerMotion::Initialize()
 {
-	motionModelMap_.insert(std::make_pair(PSTATE_WALK, Model::Load("Models/Player/Running.fbx")));
-	motionModelMap_.insert(std::make_pair(PSTATE_SHOOT, Model::Load("Models/Player/Fireball.fbx")));
-	motionModelMap_.insert(std::make_pair(PSTATE_IDLE, Model::Load("Models/Player/Silly Dancing.fbx")));
-	// モデルのロード
-	auto stageObjectList = ((Stage*)holder_)->GetStageObjects();
+    motionModelMap_.insert(std::make_pair(PSTATE_WALK, Model::Load("Models/Player/Running.fbx")));
+    motionModelMap_.insert(std::make_pair(PSTATE_SHOOT, Model::Load("Models/Player/Fireball.fbx")));
+    motionModelMap_.insert(std::make_pair(PSTATE_IDLE, Model::Load("Models/Player/Silly Dancing.fbx")));
+    // モデルのロード
+    auto stageObjectList = ((Stage*)holder_)->GetStageObjects();
 }
 
 void Component_PlayerMotion::Update()
 {
-	if (parent_ == nullptr) return;
-	// 現在のStateを取得
-	state_ = ((Component_PlayerBehavior*)parent_)->GetState();
-	static bool prevAnim = false;
+    if (parent_ == nullptr) return;
 
-	switch (state_)
-	{
-	case PSTATE_IDLE:
-		((StageObject*)holder_)->SetModelHandle(motionModelMap_[PSTATE_IDLE]);
-		if (prevAnim == false)holder_->PlayAnimation(0, 200, 1);
-		prevAnim = true; break;
+    // 現在のStateを取得
+    state_ = ((Component_PlayerBehavior*)parent_)->GetState();
 
-	case PSTATE_WALK:
+    switch (state_)
+    {
+    case PSTATE_IDLE:
+        if (((StageObject*)holder_)->GetModelHandle() != motionModelMap_[PSTATE_IDLE]) {
+            ((StageObject*)holder_)->SetModelHandle(motionModelMap_[PSTATE_IDLE]);
+            currentFrame_ = 0;  // モデルが変わったときにフレームをリセット
+        }
+        animationEndFrame_ = 200;
+        if (currentFrame_ == 0) {
+            holder_->PlayAnimation(0, animationEndFrame_, 1);
+        }
+        break;
 
-		((StageObject*)holder_)->SetModelHandle(motionModelMap_[PSTATE_WALK]);
-		if (prevAnim == false) holder_->PlayAnimation(0, 40, 1);
-		prevAnim = true;
-		break;
-	case PSTATE_SHOOT:
-		((StageObject*)holder_)->SetModelHandle(motionModelMap_[PSTATE_SHOOT]);
-		if (prevAnim == false)holder_->PlayAnimation(0, 101, 1);
-		prevAnim = true;
-		break;
-	default:
+    case PSTATE_WALK:
+        if (((StageObject*)holder_)->GetModelHandle() != motionModelMap_[PSTATE_WALK]) {
+            ((StageObject*)holder_)->SetModelHandle(motionModelMap_[PSTATE_WALK]);
+            currentFrame_ = 0;  // モデルが変わったときにフレームをリセット
+        }
+        animationEndFrame_ = 40;
+        if (currentFrame_ == 0) {
+            holder_->PlayAnimation(0, animationEndFrame_, 1);
+        }
+        break;
 
-		prevAnim = false;
-		holder_->PlayAnimation(0, 0, 1);
-	}
-	ImGui::Text("state_: %d", state_);
-	ImGui::Text("nowModelHandle : %d", ((StageObject*)holder_)->GetModelHandle());
+    case PSTATE_SHOOT:
+        if (((StageObject*)holder_)->GetModelHandle() != motionModelMap_[PSTATE_SHOOT]) {
+            ((StageObject*)holder_)->SetModelHandle(motionModelMap_[PSTATE_SHOOT]);
+            currentFrame_ = 0;  // モデルが変わったときにフレームをリセット
+        }
+        animationEndFrame_ = 101;
+        if (currentFrame_ == 0) {
+            holder_->PlayAnimation(0, animationEndFrame_, 1);
+        }
+        break;
+    }
+
+    // 現在のフレームをインクリメント
+    currentFrame_++;
+    if (currentFrame_ >= animationEndFrame_) {
+        currentFrame_ = 0;
+    }
+
+    ImGui::Text("state_: %d", state_);
+    ImGui::Text("nowModelHandle : %d", ((StageObject*)holder_)->GetModelHandle());
+    ImGui::Text("currentFrame_ : %d", currentFrame_);
 }
 
 void Component_PlayerMotion::Release()
@@ -57,8 +77,4 @@ void Component_PlayerMotion::Release()
 
 void Component_PlayerMotion::DrawData()
 {
-
 }
-
-// playerStateのアドレスを取得
-// 
--- Game/Objects/Stage/Components/MotionComponent/Component_PlayerMotion.h
+++ Game/Objects/Stage/Components/MotionComponent/Component_PlayerMotion.h
@@ -8,6 +8,8 @@ class Component_PlayerMotion : public Component_Motion
 private:
 	PlayerState state_;
 	Component_PlayerBehavior* playerBehavior_;
+	int animationEndFrame_ = 0;
+	int currentFrame_ = 0;
 public:
 	Component_PlayerMotion(string _name, StageObject* _holder, Component* _parent);
 	void Initialize() override;

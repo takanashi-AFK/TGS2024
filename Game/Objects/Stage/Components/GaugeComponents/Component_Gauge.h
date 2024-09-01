ae1ab56fbe11785c05172b87e9e4d2957b47aacb
sugawara yuto <117239251+nattoSG09@users.noreply.github.com>
2024-09-02 04:49:51 +0900

    Healthゲージコンポーネントやゲージコンポーネントの気持ち悪いコード修正

--- Game/Objects/Stage/Components/GaugeComponents/Component_Gauge.h
+++ Game/Objects/Stage/Components/GaugeComponents/Component_Gauge.h
@@ -8,35 +8,21 @@
 /// </summary>
 class Component_Gauge :public Component
 {
-private:
-
-	float maxGauge_; //ゲージの上限
-	float nowGauge_;  //現在のゲージ
+protected:
+	float max_;	// 最大値
+	float now_;	// 現在値
 
 public:
-	/// <summary>
-	/// コンストラクタ
-	/// </summary>
-	/// <param name="_holder">保有者</param>
 	Component_Gauge(StageObject* _holder, string _name, ComponentType _type, Component* _parent);
-	/// <summary>
-	/// HPの値を取得
-	/// </summary>
-	/// <returns></returns>
-	float GetNow() const { return nowGauge_; }
 
-	/// <summary>
-	/// HPの値を設定
-	/// </summary>
-	/// <param name="_newHP">与えられたHP</param>
-	void SetNow(float _newNow) { nowGauge_ = _newNow; }
+	void Save(json& _saveObj) override;
+	void Load(json& _loadObj) override;
+	void DrawData() override;
 
-	/// <returns>体力の最大値</returns>
-	float GetMax() const { return maxGauge_; }
+	float GetNow() const { return now_; }
+	void SetNow(float _newNow) { now_ = _newNow; }
+	float GetMax() const { return max_; }
+	void SetMax(float _newMax) { max_ = _newMax; }
 
-	/// <summary>
-	/// 体力の最大値を設定
-	/// </summary>
-	void SetMax(float _newMax) { maxGauge_ = _newMax; }
-
+	void AdjustToMax() { now_ = max_; }
 };

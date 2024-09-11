0ca452e8daef097a41fbb2fd527f685504eafd08
悠人 菅原 <220369@jc-21.jp>
2024-09-11 12:41:22 +0900

    // UITimerちょっと作った

--- Game/Objects/UI/UITimer.cpp
+++ Game/Objects/UI/UITimer.cpp
@@ -1,8 +1,9 @@
 #include "UITimer.h"
+#include "../../../Engine/ImGui/imgui.h"
 
 
 UITimer::UITimer(string _name, UIObject* parent, int _layerNum)
-	: UIObject(_name, UI_TIMER, parent, _layerNum)
+	: UIObject(_name, UI_TIMER, parent, _layerNum), max_(0), count_(0), isActive_(false)
 {
 }
 
@@ -12,6 +13,14 @@ void UITimer::Initialize()
 
 void UITimer::Update()
 {
+	// カウンターを更新
+	if(isActive_)count_--;
+
+	// カウンターが0になったら非アクティブにする
+	if (count_ <= 0){
+		count_ = 0;
+		isActive_ = false;
+	}
 }
 
 void UITimer::Draw()
@@ -24,8 +33,19 @@ void UITimer::Release()
 
 void UITimer::Save(json& saveObj)
 {
+	// 最大値の保存
+	saveObj["max"] = max_;
 }
 
 void UITimer::Load(json& loadObj)
 {
+	// 最大値の読み込み
+	max_ = loadObj["max"];
+	count_ = max_;
+}
+
+void UITimer::DrawData()
+{
+	// 最大値の設定
+	ImGui::DragFloat("max", &max_, 1.f, 1.f, 1000.f);
 }
--- Game/Objects/UI/UITimer.h
+++ Game/Objects/UI/UITimer.h
@@ -3,6 +3,11 @@
 
 class UITimer : public UIObject
 {
+private:
+	float count_;	// 現在値
+	float max_;		// 最大値
+
+	bool isActive_;	// タイマーが動いているか
 public:
 	/// <summary> コンストラクタ </summary>
 	UITimer(string _name, UIObject* parent, int _layerNum);
@@ -24,5 +29,40 @@ public:
 
 	/// <summary> 読込 </summary>
 	void Load(json& loadObj) override;
+
+	/// <summary> ImGui表示 </summary>
+	void DrawData() override;
+
+	/// <summary> タイマーの開始 </summary>
+	void StartTimer() { isActive_ = true; }
+
+	/// <summary> タイマーの停止 </summary>
+	void StopTimer() { isActive_ = false; }
+
+	/// <summary> タイマーの終了 </summary>
+	void EndTimer() { count_ = 0; isActive_ = false; }
+
+/*
+setter :*/
+	/// <summary> タイマーの設定 </summary>
+	void SetTimer(float _max) { max_ = _max; count_ = max_; }
+
+	/// <summary> タイマーのリセット </summary>
+	void ResetTimer() { count_ = max_; isActive_ = false; }
+/*
+getter :*/
+	/// <summary> タイマーの取得 </summary>
+	float GetTimer() const { return count_; }
+
+	/// <summary> 最大値の取得 </summary>
+	float GetMaxTimer() const { return max_; }
+
+/*
+predicate :*/
+	/// <summary> タイマーが0になったか </summary>
+	bool IsEnd() const { return count_ <= 0; }
+
+	/// <summary> タイマーが最大値か </summary>
+	bool IsMax() const { return count_ >= max_; }
 };
 
